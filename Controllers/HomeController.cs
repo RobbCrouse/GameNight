ec025ff8a9730cb030f1532fff213d8edb591367
RobbCrouse <robbcrouse@gmail.com>
2018-08-16 13:48:25 -0700

    Added Home and Game CONTROLLERS, added Index, Register, Login, and Success VIEWS, added images

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,35 +3,92 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using gamenight.Models;
 
 namespace gamenight.Controllers
 {
     public class HomeController : Controller
     {
+        private GameNightContext _context;
+        public HomeController( GameNightContext context )
+        {
+            _context = context;
+        }
+        
+        
         public IActionResult Index()
         {
-            return View();
+            return View( "Index" );
         }
 
-        public IActionResult About()
+        public IActionResult RegisterNow()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            return View( "RegisterPage" );
         }
 
-        public IActionResult Contact()
+        public IActionResult RegisterForm( UserViewModel model )
         {
-            ViewData["Message"] = "Your contact page.";
+            if( ModelState.IsValid )
+            {
+                PasswordHasher<UserViewModel> Hasher = new PasswordHasher<UserViewModel>();
+                model.Password = Hasher.HashPassword( model, model.Password );
 
-            return View();
+                User NewUser = new User
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Password = model.Password,
+                    GamerTag = model.GamerTag,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                };
+                //Handle Success
+                _context.Users.Add( NewUser );
+                _context.SaveChanges();
+
+                NewUser = _context.Users.SingleOrDefault( a => a.Email == model.Email );
+                HttpContext.Session.SetInt32( "UserId", NewUser.UserId );
+                return RedirectToAction( "Dashboard", "Game" );// Method, Controller
+            }
+            return View( "Index" );
         }
 
-        public IActionResult Error()
+
+
+        public IActionResult LoginForm( UserLogViewModel model )
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if( ModelState.IsValid )
+            {
+                User ReturnUser = _context.Users.SingleOrDefault( u => u.Email == model.Email );
+                if( ReturnUser != null )
+                {
+                    if( model.Email != null && model.Password != null )
+                    {
+                        var Hasher = new PasswordHasher<UserLogViewModel>();
+                        if( 0 != Hasher.VerifyHashedPassword( model, ReturnUser.Password, model.Password ))
+                        {
+                            HttpContext.Session.SetInt32( "UserId", ReturnUser.UserId );
+                            return RedirectToAction( "Dashboard", "Game" );
+                        }
+                    }
+                }
+                ViewBag.Login = "Please Register.";
+                return View( "Index" );
+            }
+            return View( "Index" );
         }
+
+
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Clear();
+            return View( "Index" );
+        }
+
     }
 }
