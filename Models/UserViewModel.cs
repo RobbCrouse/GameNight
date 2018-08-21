515c9c2e6115dd671755204832596598a6bf4ba1
RobbCrouse <robbcrouse@gmail.com>
2018-08-21 12:55:53 -0700

    MVP reached. Final styling and added images for ReadMe.

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -13,22 +13,22 @@ namespace gamenight.Models
         [Required( ErrorMessage = "First Name is required.")]
         [MinLength(2)]
         [RegularExpression(@"^[a-zA-Z]+$")]
-        [Display(Prompt = "First Name ")]
+        [Display(Prompt = "First Name")]
         public string FirstName { get; set; }
 
 
         [Required( ErrorMessage = "Last Name is required.")]
         [MinLength(2)]
         [RegularExpression(@"^[a-zA-Z]+$")]
-        [Display(Prompt = "Last Name ")]
+        [Display(Prompt = "Last Name")]
         public string LastName { get; set; }
 
 
-        [Display(Prompt = "Gamertag ")]
+        [Display(Prompt = "Gamertag")]
         public string GamerTag { get; set; }
 
 
-        [Display(Prompt = "Email ")]
+        [Display(Prompt = "Email")]
         [Required( ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
@@ -37,13 +37,13 @@ namespace gamenight.Models
         [Required( ErrorMessage = "Password is required.")]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
         //[RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", ErrorMessage = "Password must contain at least 1 number, 1 letter, and 1 special character.")]
-        [Display(Prompt = "Password ")]
+        [Display(Prompt = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Compare("Password", ErrorMessage = "Password and confirmation must match.")]
         //[RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", ErrorMessage = "Password must contain at least 1 number, 1 letter, and 1 special character.")]
-        [Display(Prompt = "Confirm Password ")]
+        [Display(Prompt = "Confirm Password")]
         [DataType(DataType.Password)]
         public string PasswordConfirmation { get; set; }
 
